53fabaf6f261664f74d9e680090b1c44667a5d0f
Khushali Dudhabade <khushalidudhabade27@gmail.com>
2024-07-24 13:02:07 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,51 +1,46 @@
-
 import React, { useState } from "react";
-
 import './../styles/App.css';
 
 const App = () => {
+  const [input, setInput] = useState("");
+  const [tasks, setTasks] = useState([]);
 
-    const[input,setInput]=useState("");
-    const[task,setTask]=useState([]);
+  const handleInput = (e) => {
+    setInput(e.target.value);
+  }
 
-    const handleInput=(e)=>{
-           setInput(e.target.value)
+  const addTask = (e) => {
+    e.preventDefault();
+    if (input.trim() !== "") {
+      setTasks([...tasks, input.trim()]);
+      setInput(""); 
     }
+  }
 
-    const addTask=(e)=>{
-        e.preventDefault();
-        if(input.trim()!==""){
-            setTask([...task,input])
-            
-        } 
-        
-    }
+  const deleteTask = (index) => {
+    const newTasks = tasks.slice();
+    newTasks.splice(index, 1);
+    setTasks(newTasks);
+  }
 
-    const deleteTask=(index)=>{
-        const newTasks = task.slice();
-       newTasks.splice(index, 1);
-       setTask(newTasks);
-    }
-  
   return (
     <div className="App">
-        
-        <form onSubmit={addTask}>
+      <form onSubmit={addTask}>
         <h1>Add To-Do List</h1>
-        <input type="text" onChange={handleInput}/> &nbsp;
+        <input type="text" value={input} onChange={handleInput} /> &nbsp;
         <button type="submit">Add</button>
-        </form>
-        <div>
-            <ol>
-                {
-                task.map((item,index) =>{
-                    return <li> {item}  <button onClick={() => deleteTask(index)}>Delete</button></li>
-                })
-                }
-            </ol>
-        </div>
+      </form>
+      <div>
+        <ol>
+          {tasks.map((item, index) => (
+            <li key={index}>
+              {item} <button onClick={() => deleteTask(index)}>Delete</button>
+            </li>
+          ))}
+        </ol>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
